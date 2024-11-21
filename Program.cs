804a9483772de0f44d39af3228bf58510d16f442
Displayray <fabian.degenfelder@gmail.com>
2024-11-21 17:00:19 +0100

    Übungsaufgabe z2b

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
-﻿int maxSterne = 40;
-int space = maxSterne / 2;
+﻿Console.Write("Anzahl Sterne: ");
+int n = Convert.ToInt16(Console.ReadLine());
 
-for (int sterneanzahl = 1; sterneanzahl <= maxSterne; sterneanzahl += 2)
+for(int i = 1; i <= n; i++)
 {
-    for (int i = 0; i < space; i++)
+    int sterne = 2;
+    if (i == 1) sterne = 1;
+    else if (i == n) sterne = n;
+    int offset = i-2;
+    int punkte = n - i;
+    if (offset > 0) punkte = ((n - sterne) - offset);
+    
+    for(int xi = 0; xi < sterne; xi++)
     {
-        Console.Write(" ");
+        Console.Write("*");
+        if(offset > 0 && xi == 0 && i != n)
+        {
+            for(int x = 0; x < offset; x++)
+            {
+                Console.Write(".");
+            }
+            offset--;
+        }
     }
-    for (int i = 0; i < sterneanzahl; i++)
+    
+    for(; punkte > 0; punkte--)
+    {
+        Console.Write(".");
+    }
+    
+    Console.WriteLine();
+}
+
+/* 
+    for(int xi = 0; xi < i; xi++)
     {
         Console.Write("*");
     }
+    for(int punkte = n-i; punkte > 0; punkte--)
+    {
+        Console.Write(".");
+    }
     Console.WriteLine();
-    space--;
-}
+*/
