d0817379cca98ea427e0deeb332775a06183dc1c
Displayray <fabian.degenfelder@gmail.com>
2024-11-21 16:32:21 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int maxSterne = 40;
+int space = maxSterne / 2;
+
+for (int sterneanzahl = 1; sterneanzahl <= maxSterne; sterneanzahl += 2)
+{
+    for (int i = 0; i < space; i++)
+    {
+        Console.Write(" ");
+    }
+    for (int i = 0; i < sterneanzahl; i++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+    space--;
+}
